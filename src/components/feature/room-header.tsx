5d91796b7d493606af3a8299439d4abe6fae08d7
deepak_shm <deepak.sharma@axeno.co>
2025-01-04 01:58:09 +0530

    Updated icon, copy link

--- src/components/feature/room-header.tsx
+++ src/components/feature/room-header.tsx
@@ -4,6 +4,15 @@ import { RoomContext } from "../provider/room-provider";
 
 function RoomHeader() {
   const { currPlayer, data } = useContext(RoomContext);
+  const copyRoomLink = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    (e.target as HTMLButtonElement).innerText = "copied!";
+    navigator.clipboard.writeText(window.location.href);
+    setTimeout(() => {
+      (e.target as HTMLButtonElement).innerText = "copy link";
+    }, 2000);
+  };
+
   return (
     <header className="px-4 py-2 flex flex-row max-w-7xl mx-auto bg-background items-center justify-between">
       <div className="">
@@ -22,6 +31,12 @@ function RoomHeader() {
       <div>
         {currPlayer && (
           <div className="flex flex-row items-center space-x-2">
+            <button
+              onClick={copyRoomLink}
+              className="bg-white text-black px-2 py-1 font-semibold mr-5 font-mono text-xs"
+            >
+              copy link
+            </button>
             <span className="font-minecraft">{currPlayer.nickname}</span>
           </div>
         )}
